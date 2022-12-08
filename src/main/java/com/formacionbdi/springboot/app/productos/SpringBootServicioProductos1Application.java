06a9ed20ff079b7abc9a0693b68c002cb02f1567
Jorge Meza Bello <jorberiver@gmail.com>
2022-12-07 23:52:34 -0600

    Finalizado

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringBootServicioProductos1Application.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioProductos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioProductos1Application.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Grabadora Panasonic', 2200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Televisor Sony', 8200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Refrigerador Samsung', 12500, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Horno Eléctrico Oster', 1700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Laptop Asus', 14500, NOW());
